1829daf17d2344143b492bf4acacfa5319c128ce
Y20060104 <3448658268@qq.com>
2026-05-12 19:47:04 +0800

    feat: implement three-level hierarchical timer wheel

--- src/timer_wheel.cpp
+++ src/timer_wheel.cpp
@@ -1,41 +1,111 @@
 #include "timer_wheel.h"
 
-Timer* TimerWheel::add(uint64_t delay_ticks,std::function<void()>cb)
-{
-    uint64_t expected_time = current_tick_+delay_ticks;
-    
-    Timer* new_timer=new Timer;
-    new_timer->expire_=expected_time;
-    new_timer->cancelled_=false;
-    new_timer->cb_=cb;
-    
-    
-    new_timer->next_=slots_[(expected_time)&(SLOTS_MASK)];
-    new_timer->prev_=nullptr;
-    if(slots_[(expected_time)&(SLOTS_MASK)]!=nullptr){
-        slots_[(expected_time)&(SLOTS_MASK)]->prev_=new_timer;
+Timer* TimerWheel::add(uint64_t delay_ticks, std::function<void()> cb) {
+  uint64_t expected_time = current_tick_ + delay_ticks;
+  Timer *new_timer =new Timer{nullptr, nullptr, expected_time, cb, false};
+
+  if(expected_time<256){
+    // 插入第一级
+     insert(slots_l1, expected_time & SLOTS_LEVEL1_MASK, new_timer);
+  }else if(expected_time<256*64){
+     insert(slots_l2, (expected_time >> 8) & SLOTS_LEVEL2_MASK, new_timer);
+  }else if(expected_time>=256*64){
+   insert(slots_l3, (expected_time >> 14) & SLOTS_LEVEL3_MASK, new_timer);
+  }
+return new_timer;
+}
+
+
+void TimerWheel::insert(Timer** slots,int slot,Timer* t){
+   t->next_=slots[slot];
+   t->prev_=nullptr;
+    if(slots[slot]!=nullptr){
+      slots[slot]->prev_=t;
     }
-    slots_[(expected_time)&(SLOTS_MASK)]=new_timer;
-    return new_timer;
+    slots[slot]=t;
+}
+void TimerWheel::cancel(Timer *t) { t->cancelled_ = true; }
 
+void TimerWheel::tick() {
+  current_tick_ += 1;
+ 
+  if((current_tick_&SLOTS_LEVEL1_MASK)==0){
+    if(((current_tick_)&SLOTS_LEVEL2_MASK)==0){
+      cascade(3,(current_tick_>>14)&SLOTS_LEVEL3_MASK);
+    }
+    cascade(2,(current_tick_>>8)&SLOTS_LEVEL2_MASK);
+  }
+
+   Timer* head=slots_l1[(current_tick_)&SLOTS_LEVEL1_MASK];
+  while(head){
+    if(!head->cancelled_) head->cb_();  // cancelled就跳过回调
+    Timer* next=head->next_;
+    delete head;
+    head=next;
+  }
+  slots_l1[(current_tick_)&SLOTS_LEVEL1_MASK]=nullptr;
 }
 
-void TimerWheel::cancel(Timer* t)
+void TimerWheel::cascade(int level,int slot)
 {
-    t->cancelled_=true;
-}
-
-void TimerWheel::tick()
-{
-    current_tick_+=1;
-    Timer* head=slots_[current_tick_&SLOTS_MASK];
+  switch (level)
+  {
+  case 2:
+  // 取出整个链表
+  {
+    Timer* head=slots_l2[slot];
     while(head){
-        if(!head->cancelled_){
-            head->cb_();// 回调函数
-        }
-       auto* tmp_timer=head->next_;
-        delete head;
-        head=tmp_timer;
+      Timer* next=head->next_;
+    insert(slots_l1,(head->expire_)&SLOTS_LEVEL1_MASK,head);
+      head=next;
+  }
+  slots_l2[slot]=nullptr;
+    break;
+}
+  case 3:
+  {
+ Timer* head=slots_l3[slot];
+    while(head){
+      Timer* next=head->next_;
+    insert(slots_l2,(head->expire_>>8)&SLOTS_LEVEL2_MASK,head);
+      head=next;
+  }
+  slots_l3[slot]=nullptr;
+    break;
+}
+  default:
+    break;
+  }
+}
+
+TimerWheel::~TimerWheel()
+{
+  for(int i=0;i<SLOTS_LEVEL1;++i){
+    Timer* head=slots_l1[i];
+    while(head){
+      Timer* next=head->next_;
+      delete head;
+      head=next;
     }
-    slots_[(current_tick_&SLOTS_MASK)]=nullptr;
+  }
+ 
+
+  for(int i=0;i<SLOTS_LEVEL2;++i){
+    Timer* head=slots_l2[i];
+    while(head){
+      Timer* next=head->next_;
+      delete head;
+      head=next;
+    }
+  }
+  
+  for(int i=0;i<SLOTS_LEVEL3;++i){
+    Timer* head=slots_l3[i];
+    while(head){
+      Timer* next=head->next_;
+      delete head;
+      head=next;
+    }
+  }
+  
 }
--- src/timer_wheel.h
+++ src/timer_wheel.h
@@ -2,38 +2,38 @@
 #include <cstdint>
 #include <functional>
 
-struct Timer{
-    Timer* next_;
-    Timer* prev_;
-    uint64_t expire_;
-    std::function<void()> cb_;
-    bool cancelled_;
+struct Timer {
+  Timer *next_;
+  Timer *prev_;
+  uint64_t expire_;
+  std::function<void()> cb_;
+  bool cancelled_;
 };
 
+class TimerWheel {
+public:
+ Timer* add(uint64_t delay_ticks, std::function<void()> cb);
+  void cancel(Timer *t);
+  void tick();
+  void insert(Timer** slots,int slot,Timer* t);
+  void cascade(int level,int slot);
 
+public:
+  TimerWheel() : slots_l1{}, slots_l2{}, slots_l3{}, current_tick_(0) {}
+  ~TimerWheel();
 
-class TimerWheel{
-    public:
-    Timer* add(uint64_t delay_ticks , std::function<void()> cb);
-    void cancel(Timer* t);
-    void tick();
+private:
+  static constexpr int SLOTS_LEVEL1 = 256;
+  static constexpr int SLOTS_LEVEL2 = 64;
+  static constexpr int SLOTS_LEVEL3 = 16;
 
-    public:
-    TimerWheel() : slots_l1{},slots_l2{},slots_l3{}, current_tick_(0) {}
-    private:
-    static constexpr int SLOTS_LEVEL1=256;
-     static constexpr int SLOTS_LEVEL2=64;
-     static constexpr int SLOTS_LEVEL3=16;
+  static constexpr int SLOTS_LEVEL1_MASK = 255;
+  static constexpr int SLOTS_LEVEL2_MASK = 63;
+  static constexpr int SLOTS_LEVEL3_MASK = 15;
 
+  Timer *slots_l1[SLOTS_LEVEL1];
+  Timer *slots_l2[SLOTS_LEVEL2];
+  Timer *slots_l3[SLOTS_LEVEL3];
 
-     static constexpr int SLOTS_LEVEL1_MASK=255;
-     static constexpr int SLOTS_LEVEL2_MASK=63;
-     static constexpr int SLOTS_LEVEL3_MASK=15;
-
-    Timer* slots_l1[SLOTS_LEVEL1];
-    Timer* slots_l2[SLOTS_LEVEL2];
-    Timer* slots_l3[SLOTS_LEVEL3];
-
-    uint64_t current_tick_;
-
+  uint64_t current_tick_;
 };
